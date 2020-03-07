702f24c9507b5727509623993258205838a2f3e7
Florent Forest <florent.forest9@gmail.com>
2020-03-07 18:31:44 +0100

    Fix argument name in add_weight

--- TSClusteringLayer.py
+++ TSClusteringLayer.py
@@ -42,7 +42,7 @@ class TSClusteringLayer(Layer):
         input_dim = input_shape[2]
         input_steps = input_shape[1]
         self.input_spec = InputSpec(dtype=K.floatx(), shape=(None, input_steps, input_dim))
-        self.clusters = self.add_weight((self.n_clusters, input_steps, input_dim), initializer='glorot_uniform', name='cluster_centers')
+        self.clusters = self.add_weight(shape=(self.n_clusters, input_steps, input_dim), initializer='glorot_uniform', name='cluster_centers')
         if self.initial_weights is not None:
             self.set_weights(self.initial_weights)
             del self.initial_weights
